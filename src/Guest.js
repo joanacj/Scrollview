4d975f826cf789003b3bb9eabbe6ffb5c5372786
JoanaCJ <143373361+joanacj@users.noreply.github.com>
2023-11-09 14:15:24 -0800

    ScrollView

---
+++ src/Guest.js
@@ -0,0 +1,82 @@
+import react from "react";
+
+export const guest = [
+    {
+        id: 1,
+        name: "LEPARTO, NECALY",
+        place: "Boyog sur, Hanopol "
+    },
+    {
+        id: 2,
+        name: "JULIO, ERRON",
+        place: "Boyog Norte, Hanopol"
+    },
+    {
+        id: 3,
+        name: "SUAREZ, MAE",
+        place: "Sto.Nino, Hanopol"
+    },
+    {
+        id: 3,
+        name: "CONOL, LYN",
+        place: "Hanopol Weste, Hanopol"
+    },
+    {
+        id: 3,
+        name: "CAMANZO, JOANA",
+        place: "Hanopol Norte, Hanopol"
+    },
+    {
+        id: 3,
+        name: "SUAREZ, ALLIAH SHEENA",
+        place: "Hanopol Este, Hanopol"
+    },
+    {
+        id: 3,
+        name: "CAMBAGI, MARLON",
+        place: "Hanopol Este, Hanopol"
+    },
+    {
+        id: 3,
+        name: "TABANYAG, JOHN",
+        place: "Sto.Nino, Hanopol"
+    },
+    {
+        id: 3,
+        name: "SASSY, KENNETH",
+        place: "Ambuan Mako, Hanopol"
+    },
+    {
+        id: 3,
+        name: "SANCHEZ, NELBREY",
+        place: "Cogon, Hanopol"
+    },
+    {
+        id: 3,
+        name: "BUNTAG, WARREN",
+        place: "Tagustusan, Hanopol"
+    },
+    {
+        id: 3,
+        name: "JAGUNOS, JHONMARK",
+        place: "Ambuan, Hanopol"
+    },
+    {
+        id: 3,
+        name: "DOGOY, ELMEROSE",
+        place: "Del Rosario, Hanopol"
+    },
+    {
+        id: 3,
+        name: "FELECIO, FERN ",
+        place: "Tagustusan, Hanopol"
+    },
+    {
+        id: 3,
+        name: "PIODOS, JEA ",
+        place: "Cogon, Hanopol"
+    },
+
+]
+
+export default guest;
